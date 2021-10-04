a31cb51ca83877564396260bbe8659f4de7d689e
JunHyeongB <aowlr0513@gmail.com>
2021-10-04 17:28:42 +0900

    Create Project

---
+++ src/components/LNB.vue
@@ -0,0 +1,133 @@
+<template>
+  <nav
+    ref="nav"
+    :style="{ width: `${navWidth}px` }">
+    <div class="header">
+      <div class="user-profile"></div>
+      Junhyeong's Notion
+    </div>
+    <ul>
+      <WorkspaceItem
+        v-for="workspace in workspaces"
+        :key="workspace.id"
+        :workspace="workspace" />
+    </ul>
+    <div class="actions">
+      <div
+        class="action"
+        @click="$store.dispatch('workspace/createWorkspace')">
+        <span class="material-icons">add</span>새로운 페이지
+      </div>
+    </div>
+    <div
+      ref="resizeHandle"
+      class="resize-handle"
+      @dblclick="navWidth = 240"></div>
+  </nav>
+</template>
+
+<script>
+import interact from 'interactjs'
+import WorkspaceItem from '~/components/WorkspaceItem'
+
+export default {
+  components: {
+    WorkspaceItem
+  },
+  data() {
+    return {
+      navWidth: 240
+    }
+  },
+  computed: {
+    workspaces() {
+      return this.$store.state.workspace.workspaces
+    }
+  },
+  created() {
+    this.workspacesInit()
+  },
+  mounted() {
+    this.navInit()
+  },
+  methods: {
+    async workspacesInit() {
+      await this.$store.dispatch('workspace/readWorkspaces')
+      console.log(this.$store.state.workspace.currentWorkspacePath)
+    },
+    navInit() {
+      interact(this.$refs.nav)
+        .resizable({
+          edges: {
+            right: this.$refs.resizeHandle
+          }
+        })
+        .on('resizemove', event => {
+          this.navWidth = event.rect.width
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  max-width: 500px;
+  min-width: 160px;
+  height: 100%;
+  background-color: $color-background;
+  display: flex;
+  flex-flow: column;
+  flex-shrink: 0;
+  position: relative;
+  .header {
+    padding: 14px;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    .user-profile {
+      width: 30px;
+      height: 30px;
+      border-radius: 4px;
+      margin-right: 10px;
+      background-image: url("https://cdn1.iconfinder.com/data/icons/logos-brands-in-colors/231/among-us-player-white-512.png");
+      background-size: cover;
+    }
+  }
+  ul {
+    flex-grow: 1;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+  .actions {
+    border-top: 1px solid $color-border;
+    .action {
+      height: 45px;
+      display: flex;
+      align-items: center;
+      padding: 0 14px;
+      color: $color-icon;
+      cursor: pointer;
+      &:hover {
+        background-color: $color-background--hover1;
+      }
+      .material-icons {
+        margin-right: 4px;
+        color: $color-icon;
+      }
+    }
+  }
+  .resize-handle {
+    width: 4px;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    cursor: col-resize;
+    transition: .4s;
+    &:hover {
+      background-color: $color-border;
+    }
+  }
+}
+</style>
